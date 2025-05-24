7499ef5cffea50afafaaa5c1b03398c87fda191b
Ashish <ashishr@profiledraft.com>
2025-05-24 23:46:10 +0530

    fixed padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import sampleImg from '../assets/sampleImg.png';
 const Home = () => {
   return (
     <>
-      <section className="mx-auto max-w-6xl px-8 max-lg:px-0 py-10">
+      <section className="mx-auto max-w-6xl px-8 max-lg:px-0 max-sm:pt-0 max-sm:pb-5">
         <div className="flex max-md:flex-col w-full gap-8 max-sm:gap-10 justify-center items-center">
           <div className="w-full">
             <h1 className="text-4xl font-bold sm:text-6xl break-words whitespace-normal text-primary">
               We’re Transforming the shopping experience
             </h1>
             <p className="text-xl mt-4 max-w-xl">
-              Discover a smarter, faster, and more personalized way to browse products. Say goodbye to the old hassles —
-              and hello to convenience, innovation, and style tailored just for you.
+              Discover a smarter, faster, and more personalized way to browse products. Say goodbye to the old
+              hassles — and hello to convenience, innovation, and style tailored just for you.
             </p>
             <div className="mt-8">
               <Link to="/products" className="btn btn-primary text-xl p-5">
